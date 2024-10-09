35e98aa934bb17086fb5f40e256d900782229c30
abiansyahn <abiansyahn@gmail.com>
2024-10-09 13:53:13 +0200

    adding condition for selected_button in purchase_invoice and purchase_receipt

--- attachment_mandatory_validation/public/js/purchase_invoice.js
+++ attachment_mandatory_validation/public/js/purchase_invoice.js
@@ -2,21 +2,25 @@ frappe.ui.form.on("Purchase Invoice", {
     before_workflow_action: async (frm) => {
         let promise = new Promise((resolve, reject) => {
             frappe.dom.unfreeze()
-    		frappe.call({
-                method: "attachment_mandatory_validation.attachment_mandatory.validate_attachment",
-                args: {
-                    workflow_state: frm.doc.workflow_state,
-                    doctype: frm.doc.doctype,
-                    name: frm.doc.name
-                },
-                callback: function(r) {
-                    if (r.message == true) {
-                        resolve();
-                    } else {
-                        reject();
+            if (frm.selected_workflow_action == "Confirm PO") {
+                frappe.call({
+                    method: "attachment_mandatory_validation.attachment_mandatory.validate_attachment",
+                    args: {
+                        workflow_state: frm.doc.workflow_state,
+                        doctype: frm.doc.doctype,
+                        name: frm.doc.name
+                    },
+                    callback: function(r) {
+                        if (r.message == true) {
+                            resolve();
+                        } else {
+                            reject();
+                        }
                     }
-                }
-            });
+                });
+            } else {
+                resolve();
+            }
     	});
     	await promise.catch(() => {frappe.throw(__("Attahment(s) is mandatory to proceed to next state"));});
     }
